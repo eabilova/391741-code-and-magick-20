cbd48b1554d5e54c998e4450014f70b876d985e2
Elena Abilova <lena.abilova@gmail.com>
2020-06-01 21:32:16 +0300

    Adds getRandomColor, renderTextName, renderTextScore, renderBar functions

--- js/stat.js
+++ js/stat.js
@@ -9,8 +9,8 @@ var BOARD_INNER_GAP = 25;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var FONT_HEIGHT = 20;
-var barHeight = (BOARD_HEIGHT - (BOARD_INNER_GAP * 2) - (FONT_HEIGHT * 3) - BOARD_GAP);
 
+var barHeight = (BOARD_HEIGHT - (BOARD_INNER_GAP * 2) - (FONT_HEIGHT * 3) - BOARD_GAP);
 
 var renderBoard = function (context, x, y, color) {
   context.fillStyle = color;
@@ -33,6 +33,30 @@ function rand(min, max) {
   return min + Math.random() * (max - min);
 }
 
+function getRandomColor(context, player) {
+  context.fillStyle = 'hsl(255, 100%, 49%)';
+  if (player === 'Вы') {
+    context.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    var saturation = Math.round(rand(0, 100));
+    context.fillStyle = 'hsl(255,' + saturation + '%, 49%)';
+  }
+}
+
+function renderTextName(context, player, x) {
+  context.fillStyle = '#000';
+  context.fillText(player, x, BOARD_Y + BOARD_HEIGHT - BOARD_INNER_GAP);
+}
+
+function renderTextScore(context, time, x, margin) {
+  context.fillStyle = '#000';
+  context.fillText(Math.round(time), x, margin);
+}
+
+function renderBar(context, x, margin, playerBar) {
+  context.fillRect(x, margin + BOARD_GAP, BAR_WIDTH, playerBar);
+}
+
 window.renderStatistics = function (context, players, times) {
   renderBoard(context, BOARD_X + BOARD_GAP, BOARD_Y + BOARD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderBoard(context, BOARD_X, BOARD_Y, '#ffffff');
@@ -44,19 +68,13 @@ window.renderStatistics = function (context, players, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    context.fillStyle = 'hsl(255, 100%, 49%)';
-    if (players[i] === 'Вы') {
-      context.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      var saturation = rand(0, 100);
-      context.fillStyle = 'hsl(255,' + saturation + '%, 49%)';
-    }
     var playerBar = (barHeight * times[i]) / maxTime;
     var barMargin = (barHeight - playerBar + BOARD_Y + BOARD_INNER_GAP + FONT_HEIGHT * 2);
     var positionX = BOARD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
-    context.fillRect(positionX, barMargin + BOARD_GAP, BAR_WIDTH, playerBar);
-    context.fillStyle = '#000';
-    context.fillText(players[i], positionX, BOARD_Y + BOARD_HEIGHT - BOARD_INNER_GAP);
-    context.fillText(Math.round(times[i]), positionX, barMargin);
+
+    getRandomColor(context, players[i]);
+    renderBar(context, positionX, barMargin, playerBar);
+    renderTextName(context, players[i], positionX);
+    renderTextScore(context, times[i], positionX, barMargin);
   }
 };
